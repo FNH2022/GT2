f13851bc9d4e03f53472e503daba413e81ed5269
FNH2022 <98783960+FNH2022@users.noreply.github.com>
2022-02-12 12:48:02 -0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,29 +1,32 @@
 
-import PIL
+import argparse
+import random
+
+import cv2
 import os
+import PIL
+
 from PIL import Image
 
-percent = 20  # Percent for gauge
-output_file_name = 'new_gauge.png'
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
 
+        x = 825
+        y = 825
+        loc = (x, y)
+        percent = random.randint(1, 90)  # Percent for gauge
+        output_file_name = 'new_gauge.png'
+        percent = percent / 100
+        rotation = 180 * percent  # 180 degrees because the gauge is half a circle
+        rotation = 90 - rotation  # Factor in the needle graphic pointing to 50 (90 degrees)
 
-percent = 70  # Percent for gauge
-output_file_name = 'new_gauge.png'
+        dial = Image.open('needle.png')
+        dial = dial.rotate(rotation, resample=PIL.Image.BICUBIC, center=loc)  # Rotate needle
 
-# X and Y coordinates of the center bottom of the needle starting from the top left corner
-#   of the image
-x = 825
-y = 825
-loc = (x, y)
-
-percent = percent / 100
-rotation = 180 * percent  # 180 degrees because the gauge is half a circle
-rotation = 90 - rotation  # Factor in the needle graphic pointing to 50 (90 degrees)
-
-dial = Image.open('needle.png')
-dial = dial.rotate(rotation, resample=PIL.Image.BICUBIC, center=loc)  # Rotate needle
-
-gauge = Image.open('gauge.png')
-gauge.paste(dial, mask=dial)  # Paste needle onto gauge
-gauge.save(output_file_name)
-gauge.show()
+        gauge = Image.open('gauge.png')
+        gauge.paste(dial, mask=dial)  # Paste needle onto gauge
+        gauge.save(output_file_name)    
+        gauge.imshow(output_file_name)
